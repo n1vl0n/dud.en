a4c9e83b67fa16e55314a94b2fb9fe95fa471a3f
n1vl0n <knight.n1vl0n@gmail.com>
2025-03-26 22:56:33 +0500

    fix bug; add colors

--- main.lua
+++ main.lua
@@ -2,14 +2,15 @@
 local http = require("ssl.https")
 local json = require("json")
 local url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-local data = {
-  ["text"] = 1,
-  ["definition"] = 3,
-  ["partOfSpeech"] = 3,
-  ["example"] = 3,
-}
-local bold = {
+
+local one = {
   ["partOfSpeech"] = true,
+}
+local two = {
+  ["example"] = true,
+}
+local three = {
+  ["text"] = true,
 }
 
 
@@ -19,8 +20,12 @@ function help(table)
   for key, val in pairs(table) do
     if data[key] and data[key][1] > 0 then
       data[key][1] = data[key][1] - 1
-      if bold[key] then
-        print('\27[1m'..data[key][2]..val..'\27[m')
+      if one[key] then
+        print('\27[95m\27[1m'..data[key][2]..val..'\27[m')
+      elseif two[key] then
+        print('\27[94m\27[3m'..data[key][2]..val..'\27[m')
+      elseif three[key] then
+        print('\27[92m'..data[key][2]..val..'\27[m')
       else
         print(data[key][2]..val)
       end
@@ -37,6 +42,7 @@ while true do
   data = {
     ["text"] =         {1, "  "},
     ["definition"] =   {5, "    "},
+    ["example"] =      {5, "    "},
     ["partOfSpeech"] = {5, "  "},
   }
   help(res)
